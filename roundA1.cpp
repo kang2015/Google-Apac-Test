30f419db4019aa8ef0d4bb21140e716deab2c8d8
kang2015 <kangyanli001@163.com>
2015-08-23 22:39:14 +0800

    Update roundA1.cpp

--- roundA1.cpp
+++ roundA1.cpp
@@ -12,10 +12,15 @@
 using namespace std;
 long long findPivot(long long k){
     long long num = 0;
-    while(k>0){
+    while(k>0){//don't overflow
         k = k>>1;
         num++;
     }
+    /*for(int i=1;i<=k;i++){//overflow when k is large
+        i = i<< 1;
+        cout << i<< endl;
+        num++;
+    }*/
     return 1L<<(num-1);
 }
 int main(){
